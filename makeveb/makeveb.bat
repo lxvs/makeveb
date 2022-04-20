34c6baf65b31735cd50715fdbb8e6f7500f74765
Johnny Appleseed <liuzhaohui@inspur.com>
2022-04-20 12:56:07 +0800

    makeveb 2.7.0

--- makeveb/makeveb.bat
+++ makeveb/makeveb.bat
@@ -31,8 +31,8 @@ exit /b
 ::SetDefaults
 
 :SetMetaInfo
-set "version=2.6.1"
-set "lupdate=2022-03-26"
+set "version=2.7.0"
+set "lupdate=2022-04-20"
 set "ghlink=https://github.com/islzh/makeveb"
 set "tee=%~dp0..\tee.exe"
 exit /b
